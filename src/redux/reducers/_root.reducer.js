6b690c73ce70f7993fdd0a5a50e64d33bd63f543
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-10-19 10:44:30 -0500

    Initial commit

---
+++ src/redux/reducers/_root.reducer.js
@@ -0,0 +1,40 @@
+import { combineReducers } from 'redux';
+import errors from './errors.reducer';
+import user from './user.reducer';
+import tournamentsReducer from './tournaments.reducer';
+import gamesReducer from './games.reducer';
+import selectedTournamentReducer from './selectedtournament.reducer';
+import leaderboardReducer from './leaderboard.reducer';
+import teamLeaderboardReducer from './teamLeaderboard.reducer';
+import searchTeamLeaderboardReducer from './searchTeamLeaderboard.reducer';
+import playersReducer from './players.reducer';
+import teamsReducer from './teams.reducer'
+import manageUsersReducer from './manageUsers.reducer';
+import stats from './stats.reducer';
+import teamModal from './teamsModal.reducer'
+
+// rootReducer is the primary reducer for our entire project
+// It bundles up all of the other reducers so our project can use them.
+// This is imported in index.js as rootSaga
+
+// Lets make a bigger object for our store, with the objects from our reducers.
+// This is what we get when we use 'state' inside of 'mapStateToProps'
+const rootReducer = combineReducers({
+  errors,
+  user,
+  tournamentsReducer,
+  gamesReducer,
+  selectedTournamentReducer,
+  stats,
+  leaderboardReducer,
+  teamLeaderboardReducer,
+  searchTeamLeaderboardReducer,
+  playersReducer,
+  teamsReducer,
+  manageUsersReducer,
+  teamModal,
+ 
+
+});
+
+export default rootReducer;
---
+++ src/redux/sagas/players.saga.js
@@ -0,0 +1,21 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchPlayers(action) {
+    try {
+
+        let players = yield axios.get(`api/players`);
+
+        yield put({
+            type: 'SET_PLAYERS',
+            payload: players.data
+        });
+    } catch (error) {
+        console.log('get all error', error);
+    }
+}
+
+function* playersSaga() {
+    yield takeLatest('FETCH_PLAYERS', fetchPlayers);
+}
+export default playersSaga;
