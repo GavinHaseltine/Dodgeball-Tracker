082fb86bbacbc412c0fec6d710b60482742c2d87
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-10-19 10:44:33 -0500

    Initialize presentation

---
+++ src/redux/sagas/players.saga.js
@@ -0,0 +1,19 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchPlayers() {
+    try {
+        const players = yield axios.get(`api/players`);
+        yield put({
+            type: 'SET_PLAYERS',
+            payload: players.data
+        });
+    } catch (error) {
+        console.log('get all error', error);
+    }
+}
+
+function* playersSaga() {
+    yield takeLatest('FETCH_PLAYERS', fetchPlayers);
+}
+export default playersSaga;
